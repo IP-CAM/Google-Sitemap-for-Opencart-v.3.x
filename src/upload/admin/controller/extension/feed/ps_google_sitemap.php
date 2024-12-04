c3c6500b88f49477df752be71d2e40202a64ab3d
zsoltoroszlany82 <zsolt.oroszlany@playfulsparkle.com>
2024-12-04 11:57:09 +0100

    robots.txt validation

--- src/upload/admin/controller/extension/feed/ps_google_sitemap.php
+++ src/upload/admin/controller/extension/feed/ps_google_sitemap.php
@@ -172,14 +172,11 @@ class ControllerExtensionFeedPSGoogleSitemap extends Controller
         $data['data_feed_seo_urls'] = array();
         $data['data_feed_urls'] = array();
 
-        $feed_seo_urls = array();
         $htaccess_mod = array();
 
         foreach ($languages as $language) {
             $feed_seo_url = rtrim($store_url, '/') . '/' . $language['code'] . '/sitemap.xml';
             $feed_url = rtrim($store_url, '/') . '/index.php?route=extension/feed/ps_google_sitemap&language=' . $language['code'];
-
-            $feed_seo_urls[] = $feed_seo_url;
 
             $data['data_feed_seo_urls'][$language['language_id']] = $feed_seo_url;
             $data['data_feed_urls'][$language['language_id']] = $feed_url;
@@ -189,15 +186,21 @@ class ControllerExtensionFeedPSGoogleSitemap extends Controller
 
         $data['htaccess_mod'] = implode(PHP_EOL, $htaccess_mod);
 
-        $data['robots_txt_errors'] = [];
-
-        $robotsTxtValidationResult = $this->_validateRobotsTxt($feed_seo_urls);
-
-        foreach ($robotsTxtValidationResult as $feed_seo_url => $result) {
-            if ($result) {
-                $data['robots_txt_errors'][] = sprintf($this->language->get('text_feed_url_blocked'), $feed_seo_url);
-            }
-        }
+        $data['user_agents'] = array(
+            '*' => $this->language->get('text_user_agent_any'),
+            'Googlebot' => 'Googlebot',
+            'Googlebot-Image' => 'Googlebot Image',
+            'Googlebot-Video' => 'Googlebot Video',
+            'Googlebot-News' => 'Googlebot News',
+            'Bingbot' => 'Bingbot',
+            'msnbot' => 'MSNBot',
+            'YandexBot' => 'YandexBot',
+            'YandexImages' => 'YandexImages',
+            'YandexVideo' => 'YandexVideo',
+            'YandexMedia' => 'YandexMedia',
+            'Baiduspider' => 'BaiduSpider',
+            'SeznamBot' => 'SeznamBot',
+        );
 
         $data['text_contact'] = sprintf($this->language->get('text_contact'), self::EXTENSION_EMAIL, self::EXTENSION_EMAIL, self::EXTENSION_DOC);
 
@@ -240,7 +243,7 @@ class ControllerExtensionFeedPSGoogleSitemap extends Controller
     {
 
     }
-    private function _validateRobotsTxt($urls)
+    private function _validateRobotsTxt($testUserAgent, $urls)
     {
         $results = [];
 
@@ -261,13 +264,12 @@ class ControllerExtensionFeedPSGoogleSitemap extends Controller
         // Iterate through each URL to check
         foreach ($urls as $url) {
             $parsedUrl = parse_url($url);
-            $path = $parsedUrl['path'];
+            $path = $parsedUrl['path'] . (isset($parsedUrl['query']) ? '?' . $parsedUrl['query'] : '');
 
             // Variables to track user-agent and blocking status
             $userAgent = null;
             $isBlocked = false;
             $disallowedPaths = [];
-            $defaultUserAgentFound = false;
 
             // Check each line in robots.txt
             foreach ($lines as $line) {
@@ -281,18 +283,11 @@ class ControllerExtensionFeedPSGoogleSitemap extends Controller
                 // Check if it's a User-agent directive
                 if (strpos($line, 'User-agent:') === 0) {
                     $userAgent = trim(substr($line, 11)); // Extract user-agent
-                    $defaultUserAgentFound = false;
                     continue; // Move to the next line
                 }
 
-                // If no user-agent found yet, default to Googlebot
-                if ($userAgent === null && !$defaultUserAgentFound) {
-                    $userAgent = 'Googlebot';
-                    $defaultUserAgentFound = true;
-                }
-
-                // If user-agent is Googlebot or wildcard '*', process the Disallow
-                if ($userAgent === 'Googlebot' || $userAgent === '*') {
+                // If user-agent is test user-agent or wildcard '*', process the Disallow
+                if ($userAgent === $testUserAgent || $userAgent === '*' || $userAgent === null) {
                     if (strpos($line, 'Disallow:') === 0) {
                         $disallowedPath = trim(substr($line, 9)); // Extract disallowed path
                         $disallowedPaths[] = $disallowedPath; // Store disallowed paths
@@ -310,14 +305,11 @@ class ControllerExtensionFeedPSGoogleSitemap extends Controller
             }
 
             // Store the result for this URL
-            $results[$url] = $isBlocked;
+            $results[$url] = $isBlocked ? 'text_disallowed' : 'text_allowed';
         }
 
         return $results; // Return the array of results for each URL
     }
-
-
-
 
     /**
      * Converts a Disallow pattern to a regular expression
@@ -339,6 +331,63 @@ class ControllerExtensionFeedPSGoogleSitemap extends Controller
 
         // Make sure the regular expression matches the entire path (not just a part of it)
         return '/^' . $disallowedPath . '/';
+    }
+
+    public function validaterobotstxt()
+    {
+        $this->load->language('extension/feed/ps_google_sitemap');
+
+        $json = [];
+
+        if (!$this->user->hasPermission('modify', 'extension/feed/ps_google_sitemap')) {
+            $json['error'] = $this->language->get('error_permission');
+        }
+
+        $this->load->model('localisation/language');
+
+        if (isset($this->request->get['store_id'])) {
+            $store_id = (int) $this->request->get['store_id'];
+        } else {
+            $store_id = 0;
+        }
+
+        if (isset($this->request->get['user_agent'])) {
+            $user_agent = $this->request->get['user_agent'];
+        } else {
+            $user_agent = '*';
+        }
+
+        $store_url = HTTP_CATALOG;
+
+        if (!$json) {
+
+            $feed_seo_urls = array();
+
+            $languages = $this->model_localisation_language->getLanguages();
+
+            foreach ($languages as $language) {
+                $feed_seo_urls[] = rtrim($store_url, '/') . '/index.php?route=extension/feed/ps_google_sitemap&language=' . $language['code'];
+            }
+
+            foreach ($languages as $language) {
+                $feed_seo_urls[] = rtrim($store_url, '/') . '/' . $language['code'] . '/sitemap.xml';
+            }
+
+            $results = array();
+
+            $validationResults = $this->_validateRobotsTxt($user_agent, $feed_seo_urls);
+
+            foreach ($validationResults as $feed_seo_url => $translation) {
+                $results[] = sprintf($this->language->get($translation), $feed_seo_url);
+            }
+
+            $json['results'] = implode(PHP_EOL, $results);
+        } else {
+            $json['results'] = '';
+        }
+
+        $this->response->addHeader('Content-Type: application/json');
+        $this->response->setOutput(json_encode($json));
     }
 
     private function _patchHtaccess()
--- src/upload/admin/language/es-es/extension/feed/ps_google_sitemap.php
+++ src/upload/admin/language/es-es/extension/feed/ps_google_sitemap.php
@@ -1,6 +1,7 @@
 <?php
 // Heading
 $_['heading_title']                = 'Playful Sparkle - Google Sitemap';
+$_['heading_robotstxt']            = 'Robots.txt';
 $_['heading_product']              = 'Productos';
 $_['heading_category']             = 'Categorías';
 $_['heading_manufacturer']         = 'Fabricantes';
@@ -21,6 +22,9 @@ $_['text_setup']                   = '<p><strong>Configuración del Google Sitem
 $_['text_troubleshot']             = '<ul><li><strong>Extensión:</strong> Asegúrate de que la extensión Google Sitemap esté habilitada en la configuración de OpenCart. Si la extensión está desactivada, no se generará la salida del mapa de sitio.</li><li><strong>Producto:</strong> Si las páginas de Producto no aparecen en tu mapa de sitio, asegúrate de que estén habilitadas en la configuración de la extensión y que los productos relevantes tengan su estado establecido como "Habilitado".</li><li><strong>Categoría:</strong> Si las páginas de Categoría no aparecen, verifica que las categorías estén habilitadas en la configuración de la extensión y que su estado también esté configurado como "Habilitado".</li><li><strong>Fabricante:</strong> Para las páginas de Fabricante, asegúrate de que estén habilitadas en la configuración de la extensión y que los fabricantes tengan su estado configurado como "Habilitado".</li><li><strong>Información:</strong> Si las páginas de Información no se muestran en el mapa de sitio, asegúrate de que estén habilitadas en la configuración de la extensión y que su estado esté configurado como "Habilitado".</li></ul>';
 $_['text_faq']                     = '<details><summary>¿Cómo envío mi mapa de sitio a Google Search Console?</summary>En Google Search Console, ve a <em>Mapas de sitio</em> en el menú, introduce la URL del mapa de sitio (generalmente /sitemap.xml) y haz clic en <em>Enviar</em>. Esto notificará a Google que empiece a rastrear tu sitio.</details><details><summary>¿Por qué es importante un mapa de sitio para SEO?</summary>Un mapa de sitio guía a los motores de búsqueda a las páginas más importantes de tu sitio, facilitando la indexación precisa de tu contenido, lo cual puede impactar positivamente en el ranking de búsqueda.</details><details><summary>¿Las imágenes están incluidas en el mapa de sitio?</summary>Sí, las imágenes están incluidas en el mapa de sitio generado por esta extensión, asegurando que los motores de búsqueda puedan indexar tu contenido visual junto con la URL.</details><details><summary>¿Por qué el mapa de sitio utiliza <em>lastmod</em> en lugar de <em>priority</em> y <em>changefreq</em>?</summary>Google ahora ignora los valores <priority> y <changefreq>, enfocándose en <lastmod> para la frescura del contenido. Usar <lastmod> ayuda a priorizar las actualizaciones recientes.</details>';
 $_['text_contact']                 = '<p>Para más asistencia, póngase en contacto con nuestro equipo de soporte:</p><ul><li><strong>Contacto:</strong> <a href="mailto:%s">%s</a></li><li><strong>Documentación:</strong> <a href="%s" target="_blank" rel="noopener noreferrer">Documentación para el usuario</a></li></ul>';
+$_['text_user_agent_any']          = 'Cualquier agente de usuario';
+$_['text_allowed']                 = 'Permitido: %s';
+$_['text_disallowed']              = 'Prohibido: %s';
 
 // Tab
 $_['tab_general']                  = 'General';
@@ -41,9 +45,12 @@ $_['entry_information']            = 'Información';
 $_['entry_data_feed_url']          = 'URL del feed de datos';
 $_['entry_active_store']           = 'Tienda activa';
 $_['entry_htaccess_mod']           = 'Modificación de .htaccess';
+$_['entry_validation_results']     = 'Resultados de la validación';
+$_['entry_user_agent']             = 'Agente de usuario';
 
 // Button
 $_['button_patch_htaccess']        = 'Aplicar modificación a .htaccess';
+$_['button_validate_robotstxt']    = 'Validar las reglas de Robots.txt';
 
 // Help
 $_['help_product_images']          = 'La exportación de imágenes de productos puede aumentar el tiempo de procesamiento inicialmente (solo cuando las imágenes se procesan por primera vez), y el tamaño del archivo XML sitemap será mayor como resultado.';
